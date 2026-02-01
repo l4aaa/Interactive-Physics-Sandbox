d89382131ef865f0b4b5020c7d97c2ced00959f9
Chircan Laurentiu <laurentiu.chircan@gmail.com>
2026-02-01 20:09:07 +0200

    Add rectangle shape to main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Interactive-Physics-Sandbox");
     window.setFramerateLimit(60);
+    sf::RectangleShape shape(sf::Vector2f(100.f, 100.f));
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(350.f, 250.f);
+
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -11,6 +16,7 @@ int main() {
         }
 
         window.clear(sf::Color::Black);
+        window.draw(shape);
         window.display();
     }
 
