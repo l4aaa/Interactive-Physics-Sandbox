c5c13f0e067b2ec490cca092ff29394fd73bdd47
Chircan Laurentiu <laurentiu.chircan@gmail.com>
2026-02-01 20:17:26 +0200

    Update gravity effect to use time delta for realistic movement and fixed the gravity constant to real value

--- main.cpp
+++ main.cpp
@@ -7,18 +7,21 @@ int main() {
     shape.setFillColor(sf::Color::Green);
     shape.setPosition(350.f, 250.f);
     sf::Vector2f velocity(0.f, 0.f);
-    const float gravity = 0.5f;
+    const float gravity = 981.0f;
+    sf::Clock clock;
 
 
     while (window.isOpen()) {
         sf::Event event;
+        sf::Time elapsed = clock.restart();
+        float dt = elapsed.asSeconds();
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear(sf::Color::Black);
-        velocity.y += gravity;
+        velocity.y += gravity * dt;
         shape.move(velocity);
         window.clear(sf::Color::Black);
         window.draw(shape);
